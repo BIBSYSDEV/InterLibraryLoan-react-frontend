ab8754c477360ec04a93429a1764e855b7818e97
pcb <pcb@unit.no>
2021-08-24 13:43:52 +0200

    SMILE-932-show_metadata -routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { CircularProgress, Typography } from '@material-ui/core';
-import { getMetadata } from './api/api';
-import ErrorBanner from './components/ErrorBanner';
-import styled from 'styled-components';
-import { MetaData } from './types/app.types';
-import MetadataHolder from './components/MetadataPresentation';
-
-export const StyledFullPageProgressWrapper = styled.div`
-  display: flex;
-  width: 100%;
-  min-height: 100vh;
-  padding: 0;
-  margin: 0;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const PageWrapper = styled.div`
-  width: 100%
-  padding: 0;
-  margin: 1rem;
-`;
-
-export const StyledPageTitleTypography = styled(Typography)`
-  width: 100%;
-  padding: 1rem;
-`;
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import AppContent from './AppContent';
 
 const App = () => {
-  const [metaData, setMetaData] = useState<MetaData>();
-  const [isLoadingMetaData, setIsLoadingMetaData] = useState(false);
-  const [fetchMetaDataError, setFetchMetaDataError] = useState<Error>();
-
-  //todo: read url for recordid, patronid and vid
-  const id = '1';
-
-  useEffect(() => {
-    const fetchMetadata = async () => {
-      try {
-        setIsLoadingMetaData(true);
-        setFetchMetaDataError(undefined);
-        setMetaData((await getMetadata(id)).data);
-      } catch (error) {
-        setFetchMetaDataError(error);
-      } finally {
-        setIsLoadingMetaData(false);
-      }
-    };
-    fetchMetadata().then();
-  }, []);
-
   return (
-    <>
-      {!isLoadingMetaData ? (
-        metaData && (
-          <PageWrapper>
-            <Typography variant="h1" gutterBottom>
-              Use this form to send ILL-request using NCIP
-            </Typography>
-            <MetadataHolder metaData={metaData} />
-          </PageWrapper>
-        )
-      ) : (
-        <StyledFullPageProgressWrapper>
-          <CircularProgress />
-        </StyledFullPageProgressWrapper>
-      )}
-      {fetchMetaDataError && <ErrorBanner error={fetchMetaDataError} />}
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route path="*" component={AppContent} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from 'styled-components';
 import { StylesProvider, ThemeProvider as MUIThemeProvider } from '@material-ui/styles';
 import mainTheme from './themes/mainTheme';
 import { USE_MOCK_DATA } from './utils/constants';
 import { interceptRequestsOnMock } from './api/mock-interceptors';
+import App from './App';
 
 if (USE_MOCK_DATA) {
   interceptRequestsOnMock();
