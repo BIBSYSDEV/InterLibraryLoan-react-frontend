2020d96a9a74a75baa834cc01182c488d74637a0
anetteOlli <ansi@unit.no>
2021-08-26 15:32:04 +0200

    tester bygging

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => {
   //added useless comment, more to useless
-  return <div data-testid="start">Velkommen!</div>;
+  return <div data-testid="start">Testing 123!</div>;
 };
 
 export default App;
