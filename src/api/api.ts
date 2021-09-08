e55d0a949c45aa251029ae554c0283eb93c8259a
pcb <pcb@unit.no>
2021-09-08 13:12:07 +0200

    MILE-933_sru

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,7 +7,15 @@ setAxiosDefaults();
 
 export const getMetadata = (recordId: string): Promise<AxiosResponse<MetaData>> => {
   return Axios({
-    url: encodeURI(`${API_PATHS.metadataPath}?recordid=${recordId}`),
+    url: encodeURI(`${API_PATHS.metadata}?recordid=${recordId}`),
+    method: 'GET',
+  });
+};
+export const getSRU = (recordId: string): Promise<AxiosResponse<MetaData>> => {
+  //mms_id=22257098950002203&institution=NTNU_UB&recordSchema=isohold&libraryCode=1150401
+
+  return Axios({
+    url: encodeURI(`${API_PATHS.alma}?recordid=${recordId}`),
     method: 'GET',
   });
 };
