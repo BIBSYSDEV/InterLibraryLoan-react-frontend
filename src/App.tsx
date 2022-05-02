75391d3f386623c02e1c71dbd1045652dcd3a4b8
pcb <pcb@unit.no>
2022-05-02 12:49:57 +0200

    bugfix_iframeresizer

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 import AppContent from './AppContent';
 import NotFoundPage from './components/NotFoundPage';
 import SuccessPage from './components/SucessPage';
+import { Helmet } from 'react-helmet';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Helmet>
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.6.6/iframeResizer.contentWindow.min.js"
+          type="text/javascript"
+        />
+      </Helmet>
       <Routes>
         <Route path="/" element={<AppContent />} />
         <Route path="/success" element={<SuccessPage />} />
